50a02a83c65bfe9b6020767d65dcc53a0f8753b6
victor-schumann <schumann@tutamail.com>
2022-11-11 11:18:05 +0000

    add checkpoint

--- FIcha5Exr1/main.c
+++ FIcha5Exr1/main.c
@@ -1,106 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MAXINSCRITOS 25
 #define MAXNOTA 20
 
-// --- function declaration ---
-void lerNotas (int vetorNotasFinais[], int quantAlunos);
+// DECLARACAO das FUN합ES
+int lerQuantidadeAvaliados(void);
+void lerNotas(int vetorNotasFinais[], int quantAlunos);
 int lerInteiro (int limMin, int limMax);
-int lerQuantidadeAvaliados();
-void mostrarDados(int alunos, int vetor[]);
-float calculaMedia(int vetor[], int alunos);
+void mostrarDados (int vetorNotasFinais[], int quantAlunos);
+float calculaMedia(int vetorNotasFinais[], int quantAlunos);
 
-//-- main program ---
 int main()
 {
     int vetorNotasFinais[MAXINSCRITOS];
     int quantAlunos;
     float media;
-    printf("\n\n--- Inicio do programa ---\n");
+
 
     quantAlunos = lerQuantidadeAvaliados();
+
     lerNotas(vetorNotasFinais, quantAlunos);
-    mostrarDados(quantAlunos, vetorNotasFinais);
+
+    mostrarDados(vetorNotasFinais, quantAlunos);
+
     media = calculaMedia(vetorNotasFinais, quantAlunos);
     if (media != -1)
     {
-        printf("\n\nMedia dos %d alunos avaliados: %.2f", quantAlunos, media);
+        printf("\nMEDIA das NOTAS: %.2f\n\n", media);
     }
-    printf("\n\n--- Fim do programa ---");
+
     return 0;
 }
 
-// --- function implementation ---
-alterarNotas(int vetor[], int quantAlunos) {
-    int nAluno;
-    printf("\nIndique o nr. do aluno: ");
-    nAluno = lerInteiro(0, quantAlunos);
+// -- IMPLEMENTA플O das FUN합ES ---
 
-    printf("\nNova nota do aluno: ");
-    vetorNotasFinais[nAluno-1] = lerInteiro(0, MAXNOTA);
+void alterarNota (int vetorNotasFinais[], int quantAlunos)
+{
+    int nAluno;
+
+    if (quantAlunos > 0)
+    {
+        printf("\nIndique o nr. aluno ");
+        nAluno = lerInteiro(1, quantAlunos);
+
+        printf("\nNova nota do aluno");
+        vetorNotasFinais[nAluno-1] = lerInteiro(0, MAXNOTA);
+    }
 }
 
-float calculaMedia(int vetor[], int alunos)
+
+float calculaMedia(int vetorNotasFinais[], int quantAlunos)
 {
-    int sum = 0;
-    float media = -1;
-    if (alunos > 0)
+    int somaNotas=0, i;
+    float media=-1;
+
+    for (i=0; i < quantAlunos; i++)
     {
-        for (int i=0; i<alunos; i++)
-        {
-            sum += vetor[i];
-        }
-        media = (float) sum/alunos;
+        somaNotas += vetorNotasFinais[i];
     }
+
+    if (quantAlunos != 0)
+    {
+        media = (float) somaNotas / quantAlunos;
+    }
+
     return media;
 }
 
-void mostrarDados(int alunos, int vetor[])
+
+void mostrarDados (int vetorNotasFinais[], int quantAlunos)
 {
-    if (alunos > 0)
+    int i;
+
+    printf("\n\n----- LISTAGEM das NOTAS -------");
+    if (quantAlunos == 0)
     {
-        for (int i=0; i<alunos; i++)
-        {
-            printf("\n\tNota 0%d: %d", i+1, vetor[i]);
-        }
+        printf("\nATENCAO: nao existem alunos avaliados para apresentar\n");
     }
     else
     {
-        printf("N찾o existem alunos a serem avaliados");
+        for (i=0; i < quantAlunos; i++)
+        {
+            printf("\nNOTA(%d): %d", i+1, vetorNotasFinais[i]);
+        }
     }
-
 }
 
-int lerQuantidadeAvaliados()
-{
-    int quant;
-    printf("\nIserir a quantidade de avaliados: ");
-    scanf("%d", &quant);
-    return quant;
-}
 
-int lerInteiro(int limMin, int limMax)
+int lerInteiro (int limMin, int limMax)
 {
     int num;
+
     do
     {
-        printf("\n\tvalor: ");
+        printf("\nvalor: ");
         scanf("%d", &num);
         if (num < limMin || num > limMax)
         {
-            printf("\nErro! Escolha um num. dentro do intervalo [0, %d]", MAXNOTA);
+            printf("\nERRO: o valor nao pertence ao intervalo [%d, %d]", limMin, limMax);
         }
     }
-    while (num<limMin || num>limMax);
+    while (num < limMin || num > limMax);
     return num;
 }
+
 
 void lerNotas(int vetorNotasFinais[], int quantAlunos)
 {
     int i;
+
     for (i=0; i < quantAlunos; i++)
     {
-        printf("\n\tNota 0%d: ", i+1);
+        printf("\nNota (%d): ", i+1);
         vetorNotasFinais[i] = lerInteiro(0, MAXNOTA);
     }
+}
+
+
+int lerQuantidadeAvaliados(void)
+{
+    int num;
+
+    printf("\nQuantos alunos foram avaliados: ");
+    num = lerInteiro(0, MAXINSCRITOS);
+
+    return num;
 }
